f2fb69275a278aaf0ec45235c3b5372f6f65b413
RAZU-sheikh <razu351724@gmail.com>
2023-08-06 22:34:53 +0600

    commpler arry name and add

---
+++ Module-22/Module22-3/duplicate.js
@@ -0,0 +1,11 @@
+const names = ['abul', 'babul', 'cabul', 'dabul', 'ebul',
+ 'babul', 'abul', 'kabul', 'gabul', 'cabul', 'baul', 'abul',]
+
+ function removeDuplicate(names){
+    for(let i = 0; i< names.length; i++){
+        const name = names[i];
+        console.log(name)
+    }
+ }
+  
+ const upiqueNames = removeDuplicate(names);
