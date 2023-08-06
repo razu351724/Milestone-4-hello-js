909c7ae9f514bbbc4c03723ff4f90123973147a3
RAZU-sheikh <razu351724@gmail.com>
2023-08-06 12:00:32 +0600

    i

---
+++ Module-21/Module21-5/approach.js
@@ -0,0 +1,12 @@
+// this is wrong approach
+
+let first = 5;
+let second = 7;
+console.log(first,second)
+
+// approach:2...Destrukturing 
+// [ first, second ] = [ second, first ]
+// console.log(second, first)
+
+// ঝামেলা আছে কনসলে রান হচ্ছে না
+console.log('i am razu')
