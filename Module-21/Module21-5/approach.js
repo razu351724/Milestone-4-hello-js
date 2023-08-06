d4b6fd20a6e016f2493c6252f339280073c77029
RAZU-sheikh <razu351724@gmail.com>
2023-08-06 14:15:31 +0600

    approach sestemo

--- Module-21/Module21-5/approach.js
+++ Module-21/Module21-5/approach.js
@@ -7,6 +7,3 @@ console.log(first,second)
 // approach:2...Destrukturing 
 // [ first, second ] = [ second, first ]
 // console.log(second, first)
-
-// ঝামেলা আছে কনসলে রান হচ্ছে না
-console.log('i am razu')
