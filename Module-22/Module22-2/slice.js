9f754d0a96ca24450707fe8c2fc2d2fc9da8325f
RAZU-sheikh <razu351724@gmail.com>
2023-08-06 14:10:59 +0600

    slice di cut index number

---
+++ Module-22/Module22-2/slice.js
@@ -0,0 +1,6 @@
+// এবার আমর কেচি দিয়ে কেটে ফেলবো,
+// index এর লাইন ধরে slice কাটে নেয়, 
+const friends = [ 12, 45, 32, 22, 44, 62, 29, 69, 87]
+const patial = friends.slice(2,5)
+console.log(patial);
+console.log(friends);
